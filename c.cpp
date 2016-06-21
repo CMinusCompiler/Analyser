26a8b30b686f020d1376907fe710c972c9f307f3
marc <474733787@qq.com>
2016-06-22 00:39:18 +0800

    //completion

---
+++ c.cpp
@@ -0,0 +1,6 @@
+void gcd (void)
+{  if (v == 0) return;
+else return;
+/* u-u/v*v == u mod v */
+}
+
---
+++ d.cpp
@@ -0,0 +1,26 @@
+int gcd (int u, int v)
+{  if (v == 0) return u ;
+else return gcd(v,u-u/v*v);
+/* u-u/v*v == u mod v */
+}
+int main()
+{  int x; int y;
+x = input(); y = input();
+output ( gcd ( x , y ) ) ;
+
+ x=a[3];
+ 
+  while(1)
+  {
+ 
+  if(2)
+   {}
+  else
+  if(2)
+  {}
+  else{}
+  }
+
+
+}
+
